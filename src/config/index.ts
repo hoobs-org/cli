9aef4f966d4f42cfdde1acde7ade3880fdd66e37
mkellsy <mkellsy@gmail.com>
2021-02-25 17:43:16 -0700

    make plugin package file private

--- src/config/index.ts
+++ src/config/index.ts
@@ -48,6 +48,7 @@ export default class Config {
         let pjson = {
             name: "plugins",
             description: "HOOBS Plugins",
+            private: true,
             dependencies: {},
         };
 
